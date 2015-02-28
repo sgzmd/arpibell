356b034fc2540dc01b5db45772e8c6e1a41bbb1e
Roman Kirillov <sigizmund@gmail.com>
2015-02-28 16:53:00 +0000

    Working sketch

--- arduino/arbell/arbell.ino
+++ arduino/arbell/arbell.ino
@@ -1,47 +1,70 @@
-#include <Wire.h>
+#include <EtherCard.h>
 
-#define SLAVE_ADDRESS 0x04
-int number = 0;
-int state = 0;
+// ethernet interface mac address, must be unique on the LAN
+static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
+
+byte Ethernet::buffer[700];
+static uint32_t timer;
+
+const char website[] PROGMEM = "www.google.com";
+
+// #include <Wire.h>
+
+// #define SLAVE_ADDRESS 0x05
+// int number = 0;
+// int state = 0;
 
 void setup() {
-  pinMode(13, OUTPUT);
+  // pinMode(13, OUTPUT);
   Serial.begin(9600);         // start serial for output
-  // initialize i2c as slave
-  Wire.begin(SLAVE_ADDRESS);
+  // // initialize i2c as slave
+  // Wire.begin(SLAVE_ADDRESS);
 
-  // define callbacks for i2c communication
-  Wire.onReceive(receiveData);
-  Wire.onRequest(sendData);
+  // // define callbacks for i2c communication
+  // Wire.onReceive(receiveData);
+  // Wire.onRequest(sendData);
+
+  if (ether.begin(sizeof Ethernet::buffer, mymac) == 0) 
+    Serial.println(F("Failed to access Ethernet controller"));
+  if (!ether.dhcpSetup())
+    Serial.println(F("DHCP failed"));
+
+  ether.printIp("IP:  ", ether.myip);
+  ether.printIp("GW:  ", ether.gwip);  
+  ether.printIp("DNS: ", ether.dnsip);  
+
+  if (!ether.dnsLookup(website))
+    Serial.println("DNS failed");
+    
+  ether.printIp("SRV: ", ether.hisip);
 
   Serial.println("Ready!");
 }
 
-void loop() {
-    delay(100);
+// called when the client request is complete
+static void my_callback (byte status, word off, word len) {
+  Serial.println(">>>");
+  Ethernet::buffer[off+300] = 0;
+  Serial.print((const char*) Ethernet::buffer + off);
+  Serial.println("...");
 }
 
-// callback for received data
-void receiveData(int byteCount){
+void loop() {
+  ether.packetLoop(ether.packetReceive());
+  int sensorValue = analogRead(A3);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
 
-  while(Wire.available()) {
-    number = Wire.read();
-    Serial.print("data received: ");
-    Serial.println(number);
-    if (number == 1){
-      if (state == 0){
-        digitalWrite(13, HIGH); // set the LED on
-        state = 1;
-      }
-      else{
-        digitalWrite(13, LOW); // set the LED off
-        state = 0;
-      }
-    }
+  if (sensorValue > 500) {
+    Serial.println("Ding-dong!!");
+    Serial.println(sensorValue);
+
+    Serial.println();
+    Serial.print("<<< REQ ");
+    ether.browseUrl(PSTR("/foo/"), "bar", website, my_callback);
+
+    delay(5000);
+  } else {
+    delay(100);
   }
 }
 
-// callback for sending data
-void sendData(){
-  Wire.write(number);
-}
