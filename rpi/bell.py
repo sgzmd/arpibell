1e51b58962dfd405438e5b6e615062ac3eeb826c
Roman Kirillov <me@romankirillov.info>
2015-07-19 14:08:37 +0100

    Mac notification

--- rpi/bell.py
+++ rpi/bell.py
@@ -1,4 +1,9 @@
 import socket
+import time
+
+from Foundation import NSUserNotification
+from Foundation import NSUserNotificationCenter
+from Foundation import NSUserNotificationDefaultSoundName
 
 UDP_IP = ''
 UDP_PORT = 31716
@@ -10,4 +15,12 @@ sock.setblocking(1)
 
 while True:
   data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+  notification = NSUserNotification.alloc().init()
+  notification.setTitle_("Doorbell!")
+  notification.setInformativeText_(data)
+  notification.setSoundName_(NSUserNotificationDefaultSoundName)
+
+  center = NSUserNotificationCenter.defaultUserNotificationCenter()
+  center.deliverNotification_(notification)
+  time.sleep(5)
   print "received message:", data
